106a16e318a6020246f9314308ab03b2a4ed6ce3
Fredrik Magnussen <fredrma@gmail.com>
2016-09-03 23:30:01 +0200

    main code organized into functions

--- projects/project1/project1/main.cpp
+++ projects/project1/project1/main.cpp
@@ -8,12 +8,65 @@
 
 using namespace std;
 
+void populate_vectors(int, double *, double *, double *, double *, double *, double *);
+void solve_tridiagonal(int, double *, double *, double *, double *, double *, double *, double *);
+
+// Function which populates the vectors with our given function and boundary condtitions
+void populate_vectors(int n, double *a, double *b, double *c, double *x, double *b_tilde, double *u){
+    // Intitialize variables
+    double h;
+    int i;
+
+    // Put boundary conditions
+    x[0] = 0;
+    u[0] = 0;
+    x[n+2] = 1;
+    u[n+2] = 0;
+
+    // step length
+    h = 1.0/(n+1.0);
+    // Calculate the b_tilde defined as h²*f(x_i), where f=100e^(-10x)
+    for (i = 1; i <= n+1; i++){
+        x[i]=i*h;
+        b_tilde[i]=h*h*100.0*exp(-10.0*x[i]);
+    }
+
+    // Populate arrays a, b, and, c.
+    for (i = 1; i <= n; i++){
+        a[i] = -1.0;
+        b[i] = 2.0;
+        c[i] = -1.0;
+    }
+}
+
+// Function which solve the set of linear equations when the matrix is
+// a tridiagonal matrix. The algorithm is given by Teukolsky et al,
+// and consists of a forward and backward substitution.
+void solve_tridiagonal(int n, double *a, double *b, double *c, double *b_tilde, double *u, double* temp){
+    // Declare variables
+    double btemp;
+    int i;
+
+    btemp = b[1];
+    u[1] = b_tilde[1]/btemp;
+
+    // Forward substitution
+    for (i = 2; i <= n; i++){
+        temp[i] = c[i-1]/btemp;
+        btemp = b[i] - a[i]*temp[i];
+        u[i] = (b_tilde[i]-a[i]*u[i-1])/btemp;
+    }
+
+    // Backward substitution
+    for (i = n-1; i>=1; i--){
+        u[i] -= temp[i+1]*u[i+1];
+    }
+}
+
 int main(int argc, char* argv[])
 {
     // Declare variables
-    int i, n;
-
-    double h, btemp;
+    int n;
 
     // vectors
     double *a, *b, *c, *x, *b_tilde, *u, *temp;
@@ -31,7 +84,6 @@ int main(int argc, char* argv[])
     }
 
     // Allocate vectors
-
     // The vectors a, b, and, c goes from 1:n
     // We need to allocate n+1 elements as we dont use the 0 position, i.e., a[0].
     a = new double [n+1];
@@ -45,45 +97,11 @@ int main(int argc, char* argv[])
     u = new double [n+2];
     b_tilde = new double [n+2];
 
-    // step length
-    h = 1/(n+1);
+    // Populate vectors
+    populate_vectors(n, a, b, c, x, b_tilde, u);
 
-    // Put boundary conditions
-    x[0] = 0;
-    u[0] = 0;
-    x[n+2] = 1;
-    u[n+2] = 0;
-
-    // Calculate the b_tilde defined as h²*f(x_i), where f=100e^(-10x)
-    for (i = 1; i <= n+1; i++){
-        x[i]=i*h;
-        b_tilde[i]=h*h*100*exp(-10*x[i]);
-    }
-
-    // Populate arrays a, b, and, c.
-    for (i = 1; i <= n; i++){
-        a[i] = -1;
-        b[i] = 2;
-        c[i] = -1;
-    }
-
-    // The algorithm for solvind the tridiagonal problem is given by Teukolsky et al,
-    // and consists of a forward and backward substitution.
-
-    btemp = b[1];
-    u[1] = b_tilde[1]/btemp;
-    // Forward substitution
-    for (i = 2; i <= n; i++){
-        temp[i] = c[i-1]/btemp;
-        btemp = b[i] - a[i]*temp[i];
-        u[i] = (b_tilde[i]-a[i]*u[i-1])/btemp;
-    }
-
-    // Backward substitution
-    for (i = n-1; i>=1; i--){
-        u[i] -= temp[i+1]*u[i+1];
-    }
-
+    // Solve the tridiagonal system
+    solve_tridiagonal(n, a, b, c, b_tilde, u, temp);
 
     // Deallocate vectors
     delete [] a;
